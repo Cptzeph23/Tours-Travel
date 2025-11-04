90de6a1945dec5a24087aa7083321a7491d5c3d8
Cptzeph23 <asekazephaniah@gmail.com>
2025-11-04 23:44:48 +0300

    form

--- UniQuest/travello/models.py
+++ UniQuest/travello/models.py
@@ -9,3 +9,16 @@ class Destination(models.Model):
     desc = models.TextField()
     price = models.IntegerField()
     offer = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
