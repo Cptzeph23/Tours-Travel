8beda53ea04723924baf083beb2c8b7459fe7085
Cptzeph23 <asekazephaniah@gmail.com>
2025-11-05 15:54:57 +0300

    file storage

--- UniQuest/travello/models.py
+++ UniQuest/travello/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 
 class Destination(models.Model):
 
     name =  models.CharField(max_length=100)
-    img = models.ImageField(upload_to='pics')
+    img = CloudinaryField('image')
     desc = models.TextField()
     price = models.IntegerField()
     offer = models.BooleanField(default=False)
