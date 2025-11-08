7be38465d5db3e973043b0390ce7469160d56e51
Cptzeph23 <asekazephaniah@gmail.com>
2025-11-08 22:10:18 +0300

    sitemap

--- UniQuest/travello/urls.py
+++ UniQuest/travello/urls.py
@@ -1,4 +1,6 @@
 from  django.urls import path
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
@@ -6,5 +8,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('news/', views.news, name='news'),
-    path('destinations/', views.destinations, name='destinations')
+    path('destinations/', views.destinations, name='destinations'),
+    path("sitemap.xml", TemplateView.as_view(template_name="sitemap.xml", content_type="application/xml"))
 ]
